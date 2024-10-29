4d6cca834c2800930e3595a97fb5302ddd0a37bb
mamirhashmi <mamirhashmi5@gmail.com>
2024-10-29 14:22:33 +0500

    work

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from './pages/AboutUs';
 import AppPage from './pages/AppPage';
 import ContactUs from './pages/ContactUs';
 import LogoDesign from './pages/LogoDesign';
+// <!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-...your-integrity-hash..." crossorigin="anonymous"></script>
 
 
 
