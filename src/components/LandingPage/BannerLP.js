bab298478dd192abf5281abafcb80796c636dfc3
Hamzarfs <hamzasarwar.rfs@gmail.com>
2025-01-06 21:11:52 +0500

    lp

--- src/components/LandingPage/BannerLP.js
+++ src/components/LandingPage/BannerLP.js
@@ -37,9 +37,7 @@ const Lpbanner = ({ updatePopupTitle }) => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
             // phoneRegex = /^(\+44\s?|0)\d{3}\s?\d{3}\s?\d{3,4}$/
             phoneRegex = /^[+]?[0-9]{1,4}[\s\(\)-]?[0-9]{1,4}[\s\(\)-]?[0-9]{1,4}[\s\(\)-]?[0-9]{1,4}$/
-
-
-
+            
 
 
         let html = ''
